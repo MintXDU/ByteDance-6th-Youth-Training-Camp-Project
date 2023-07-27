a9d6693cf8dfb3998a686139ed11b54bba2fe5a9
MintXDU <1104540868@qq.com>
2023-07-27 16:23:08 +0800

    修改数据库初始化文件

--- sql/douyin.sql
+++ sql/douyin.sql
@@ -1,5 +1,37 @@
 CREATE DATABASE `douyin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
+-- select database douyin
+USE douyin;
+
+-- douyin.users definition
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `follow_count` int DEFAULT NULL,
+  `follower_count` int DEFAULT NULL,
+  `is_follow` tinyint(1) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- douyin.videos definition
+
+CREATE TABLE `videos` (
+  `id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `play_url` varchar(100) DEFAULT NULL,
+  `cover_url` varchar(100) DEFAULT NULL,
+  `favorite_count` int DEFAULT NULL,
+  `comment_count` int DEFAULT NULL,
+  `is_favorite` tinyint(1) DEFAULT NULL,
+  `submission_time` varchar(100) DEFAULT NULL,
+  KEY `videos_submission_time_IDX` (`submission_time`) USING BTREE,
+  KEY `videos_FK` (`user_id`),
+  CONSTRAINT `videos_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 INSERT INTO douyin.users (name,follow_count,follower_count,is_follow,password) VALUES
 	 ('zhanglei',10,5,1,'douyin'),
 	 ('1104540868',0,0,0,'123456');
