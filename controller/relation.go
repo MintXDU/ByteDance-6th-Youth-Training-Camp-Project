c670632071a703ac1b9f9e2b242234ec6956b61c
ssccww <51026096+ssccww@users.noreply.github.com>
2023-08-06 10:57:45 +0800

    Update relation.go

--- controller/relation.go
+++ controller/relation.go
@@ -35,20 +35,80 @@ func FollowList(c *gin.Context) {
 
 // FollowerList all users have same follower list
 func FollowerList(c *gin.Context) {
-	c.JSON(http.StatusOK, UserListResponse{
-		Response: dao.Response{
-			StatusCode: 0,
-		},
-		UserList: []dao.User{DemoUser},
-	})
+	username := c.Query("username")
+	token := c.Query("token")
+
+	db := service.Connection()
+	var user dao.User
+	db.Where("name = ?", username).Find(&user)
+	if user.Name == token {
+		var userList []dao.User
+		var users dao.User
+		userId := user.Id
+		var follower []dao.Follow
+		db.Where("users_id = ?", userId).Find(&follower)
+		for i := 0; i < len(follower); i++ {
+			followerId := follower[i].FollowerId
+			var count int64
+			db.Where("users_id = ?", followerId).Find(&users)
+			if db.Model(&follower[i]).Where("users_id = ? AND follow_id = ?", followerId, userId).Count(&count); count > 0 {
+				users.IsFollow = true
+			} else {
+				users.IsFollow = false
+			}
+			userList = append(userList, users)
+		}
+		c.JSON(http.StatusOK, UserListResponse{
+			Response: dao.Response{
+				StatusCode: 0,
+			},
+			UserList: userList,
+		})
+
+	} else {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: dao.Response{StatusCode: 1, StatusMsg: "token is error!"},
+		})
+	}
+
 }
 
 // FriendList all users have same friend list
 func FriendList(c *gin.Context) {
-	c.JSON(http.StatusOK, UserListResponse{
-		Response: dao.Response{
-			StatusCode: 0,
-		},
-		UserList: []dao.User{DemoUser},
-	})
+	username := c.Query("username")
+	token := c.Query("token")
+
+	db := service.Connection()
+	var user dao.User
+	db.Where("name = ?", username).Find(&user)
+	if user.Name == token {
+		var userList []dao.User
+		var users dao.User
+		userId := user.Id
+		var friend []dao.Friend
+		var follower dao.Follow
+		db.Where("user_id = ?", userId).Find(&friend)
+		for i := 0; i < len(friend); i++ {
+			friendId := friend[i].FriendID
+			var count int64
+			db.Where("users_id = ?", friendId).Find(&users)
+			if db.Model(&follower).Where("users_id = ? AND follow_id = ?", friendId, userId).Count(&count); count > 0 {
+				users.IsFollow = true
+			} else {
+				users.IsFollow = false
+			}
+			userList = append(userList, users)
+		}
+		c.JSON(http.StatusOK, UserListResponse{
+			Response: dao.Response{
+				StatusCode: 0,
+			},
+			UserList: userList,
+		})
+
+	} else {
+		c.JSON(http.StatusOK, UserLoginResponse{
+			Response: dao.Response{StatusCode: 1, StatusMsg: "token is error!"},
+		})
+	}
 }
